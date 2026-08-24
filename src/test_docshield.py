3b0a8c3174630442a61696e5cdf01dc45e48a74f
Bruxe-Dev <mnibeza23@gmail.com>
2026-08-24 21:23:40 +0200

    Finished loading the model now going to run it and load the model.

---
+++ src/test_docshield.py
@@ -0,0 +1,31 @@
+import torch
+from torchgen.api.types import bfloat16T
+from transformers import AutoProcessor, AutoTokenizer, AutoModelForImageTextToText
+from qwen_vl_utils import process_vision_info
+
+MODEL  = "vankey/DocShield-9B"
+
+print("Loading model...")
+processor = AutoProcessor.from_pretrained(
+    MODEL,
+    trust_remote_code=True,
+)
+
+print("Loading tokenizer...")
+tokenizer = AutoTokenizer.from_pretrained(
+    MODEL,
+    trust_remote_code=True
+)
+
+print("Loading model...")
+model = AutoModelForImageTextToText.from_pretrained(
+    MODEL,
+    trust_remote_code=True,
+    torch_dtype= bfloat16T,
+    device_map= "auto"
+)
+
+model.eval()
+
+print("DocShield loaded Successfully!")
+print(f"Device: {model.device}")
