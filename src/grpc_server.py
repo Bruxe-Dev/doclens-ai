c1ce5f5b0e10958b6e00d8f5c1bf2da96b605eba
Bruxe-Dev <mnibeza23@gmail.com>
2026-08-25 23:54:37 +0200

    Finished hte first version for the MVP but ther are some tcp handshake problems that needs deep insections nad look into.

--- src/grpc_server.py
+++ src/grpc_server.py
@@ -40,7 +40,7 @@ def serve():
     doclens_pb2_grpc.add_DocumentAnalysisServicer_to_server(
         DocumentAnalysisServicer(), server
     )
-    server.add_insecure_port('[0.0.0.0:50051')
+    server.add_insecure_port('0.0.0.0:50051')
     print("DocLens AI gRPC server running on port 50051...")
     server.wait_for_termination()
 
